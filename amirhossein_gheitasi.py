2cd669c6d6f9b05b5ecca58c50135c21dac7d9da
Amirhossein <amirho3ein8593@gmail.com>
2025-08-03 10:18:51 +0330

    window add

--- amirhossein_gheitasi.py
+++ amirhossein_gheitasi.py
@@ -1,37 +1,43 @@
-import re
+from  tkinter import *
+from tkinter import StringVar
+from tkinter.messagebox import showinfo
 
-def name_checker(name):
-    if re.match(r"^[a-zA-z\s]{3,30}$",name):
-        return True
-    else:
-        return False
+#from amirhossein_gheitasi import *
+product_list=[]
 
-def family_checker(family):
-    if re.match(r"^[a-zA-z\s]{3,30}$",family):
-        return True
-    else:
-        return False
+def save_clik():
+    #if name_validator(name.get()) and name_validator(quantity.get())and name_validator
+     product = {"name": name.get(),"quantity": quantity.get(),"price": price.get()}
+     product_list.append(product)
+     print(product_list)
+     name.set("")
+     quantity.set(0)
+     price.set(0)
+     showinfo("save","product saved")
+     count.set(len(product_list))
 
-def address_checker(address):
-    if re.match(r"^[a-zA-Z0-9]{5,80}$",address):
-        return True
-    else:
-        return False
+window=Tk()
 
-def mobile_checker(mobile):
-    if re.match(r"^+98/09[0-9]{9}$",mobile):
-        return True
-    else:
-        return False
+window.title("product")
+window.geometry("250x250")
 
-def email_checker(email):
-    if re.match(r"^[a-zA-z0-9]{6,30}@gmail.com$",email):
-        return True
-    else:
-        return False
+#name
+Label(window,text="name").place(x=20,y=20)
+name=StringVar()
+Entry(window,textvariable=name).place(x=80,y=20)
 
-def code_meli_checker(code_meli):
-    if re.match(r"^[0-9]{10}$",code_meli):
-        return True
-    else:
-        return False
+#quantity
+Label(window,text="quantity").place(x=20,y=60)
+quantity=IntVar(value=0)
+Entry(window,textvariable=quantity).place(x=80,y=60)
+
+#price
+Label(window,text="price").place(x=20,y=100)
+price=IntVar()
+Entry(window,textvariable=price).place(x=80,y=100)
+
+count=StringVar(value="count=0")
+Label(window,textvariable=count,text="count").place(x=30,y=150)
+Button(window,text="save",width=10,command=save_clik).place(x=100,y=150)
+
+window.mainloop()
